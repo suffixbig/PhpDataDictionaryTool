111bc080c8d7ad8d0fca78178d7c6e1965527322
suffixbig <suffixbig@gmail.com>
2017-05-10 18:07:56 +0800

    0.2.0.7
    小修正

--- ajax/goto_datasheet.php
+++ ajax/goto_datasheet.php
@@ -7,8 +7,8 @@ require($thisDir."/config.inc.mysql.php" ); //載入資料庫帳號設定檔
 require_once INCLUDE_PATH."/mysql.inc.php";     // 載入資料庫函式
 require_once INCLUDE_PATH."/global_suffix.php";     // 載入資料庫函式
 /***********************************************************************************************/
-
-$dblink=_mysql_open();//開資料庫連線================================================
+$db = new Api_mysqli;
+$dblink = $db->mysql_open(); //開資料庫連線================================================
 
 //$database='cart_opencart_00';//指定資料庫
 //$v_TABLE_NAME="oc_product";//指定資料表
@@ -26,14 +26,14 @@ if (empty($_GET['tablename'])) {
 //===========================================================
 
 if ($database) {
-    mysqluse($dblink, $database);//切換資料庫
+    $db->mysqluse($database,$dblink); //切換資料庫
 }
 //===========================================================
 $sql = 'SELECT TABLE_COMMENT FROM ';
 $sql .= 'INFORMATION_SCHEMA.TABLES ';
 $sql .= 'WHERE ';
 $sql .= "table_name = '{$v_TABLE_NAME}'  AND table_schema = '{$database}'";
-$v[ 'TABLE_COMMENT' ]=row_sql1p($sql, 0, $dblink);
+$v[ 'TABLE_COMMENT' ]=$db->row_sql1p($sql, 0, $dblink);
 
 
 $sql = 'SELECT * FROM ';
@@ -41,13 +41,13 @@ $sql .= 'INFORMATION_SCHEMA.COLUMNS ';
 $sql .= 'WHERE ';
 $sql .= "table_name = '{$v_TABLE_NAME}' AND table_schema = '{$database}'";
 
-$v[ 'COLUMN' ]=assoc_sql($sql, $dblink);
+$v[ 'COLUMN' ]=$db->assoc_sql($sql, $dblink);
 /*
 print_r($TABLE_COMMENT);
 print_r($date);
 exit;
 */
-_mysql_close($dblink);//關資料庫==============================================================
+$db->mysql_close($dblink); //關資料庫==============================================================
 //print_r($tables);
 /***********************************************************************************************/
 
--- ajax/goto_edit.php
+++ ajax/goto_edit.php
@@ -7,17 +7,31 @@ require($thisDir."/config.inc.mysql.php" ); //載入資料庫帳號設定檔
 require_once INCLUDE_PATH."/mysql.inc.php";     // 載入資料庫函式
 require_once INCLUDE_PATH."/global_suffix.php";     // 載入資料庫函式
 /***********************************************************************************************/
+//http://test.com/github_cloud/PhpDataDictionaryTool/ajax/goto_edit.php?database=cart_opencart_00&tablename=oc_affiliate&edittext=abc
+/***********************************************************************************************/
 $err_ok = 1;//缺少參數要失敗
 $r[ 'sms' ] = '修改失敗';
-//print_r($_POST);
 if(isset($_POST['database']) && isset($_POST['tablename']) &&  isset($_POST['edittext']) ) {
 
 	$database=trim($_POST['database']);
 	$tablename=trim($_POST['tablename']);
 	$edittext=trim($_POST['edittext']); 
 	if ($database && $tablename && $edittext){
-		$err_ok = 0;//缺少參數要失敗
-		
+$db = new Api_mysqli;
+$dblink = $db->mysql_open(); //開資料庫連線================================================
+//===========================================================
+if ($database) {
+    $db->mysqluse($database,$dblink); //切換資料庫
+	//修改註解
+	$sql="ALTER TABLE `".$tablename."` ";
+	$sql.="COMMENT='".$edittext."' ";
+	$ok=$db->_sql($sql,$dblink);//成功不會有返回值
+	$err_ok = 0;//參數都有
+	$r[ 'sms' ] = '修改成功';
+}
+//===========================================================
+//有指定資料庫 情況 END
+$db->mysql_close($dblink); //關資料庫========================================================
 	}else{
 		$err_ok = 1;//缺少參數要失敗
 		$r[ 'sms' ] = '沒填內容你要改啥';
