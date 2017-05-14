e8526df4d94645af7f6029b0656a76a6b09713c2
No.10 <suffixbig@gmail.com>
2017-05-15 00:07:13 +0800

    0.2.1.6
    小修正

--- config.inc.mysql.php
+++ config.inc.mysql.php
@@ -59,6 +59,7 @@ $no_show_table = array(); //不需要顯示的數據表
 //在bkk.tw這個網址下時使用特別設定
 if(preg_match("@bkk.tw@",$_SERVER['HTTP_HOST']) or preg_match("@m729499.com@",$_SERVER['HTTP_HOST']) ){
 	$cfg['servers'][$i]['port'] = 33306;//端口修改
+	$cfg['servers'][$i]['database'] = 'coachmin';      //預設開啟資料庫
 	$ok_show_databases =array('cart_opencart_00','credit','coachmin');//只能顯示的數據庫
 }
 
